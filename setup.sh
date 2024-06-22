bfa621e86836c9d5fc1614fbd24718ce9f289205
Stardust Song <6882971+ssghost@users.noreply.github.com>
2024-06-22 17:38:14 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+tailscale up --advertise-connector --advertise-tags=tag:connector &&
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf &&
+echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf &&
+sudo sysctl -p /etc/sysctl.conf
